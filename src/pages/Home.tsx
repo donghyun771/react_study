a3ccc9052bec19ee211d510dd8084f3f9211fe7f
donghyun771 <dh031208@naver.com>
2024-01-28 15:02:05 +0900

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,35 @@
 import TodoView from "./TodoView";
 import { useCallback, useState } from "react";
 
+interface TodoProps {
+  id: number;
+  name: string;
+}
+
 function Home() {
   const [todoContent, setTodoContent] = useState("");
-  const [todoView, setTodoView] = useState<string[]>([]);
+  // const [todoView, setTodoView] = useState<string[]>([]);
+  const [todoView, setTodoView] = useState<TodoProps[]>([]);
 
   const addTodoView = useCallback(() => {
+
+    const todo = [...todoView]
+
+    console.log(todo[todo.length-1].id + 1)
+
     // 이전 상태값과 현재 입력값을 합친 배열을 set
-    setTodoView([...todoView, todoContent]);
+    setTodoView([...todoView , {id : todo[todo.length-1].id + 1, name : todoContent}]);
     // 입력 필드 비우기
     setTodoContent("");
   }, [todoContent, todoView]);
+
+  const todoDelete = useCallback((id: number) => {
+    const deleted = todoView.filter((todo) => (
+        todo.id !== id
+    ))
+    setTodoView(deleted)
+
+  }, [todoView])
 
   return (
     <div>
@@ -25,7 +44,11 @@ function Home() {
       <br />
       <button
         onClick={() => {
-          addTodoView();
+          if (todoContent === "") {
+            alert("내용이 없습니다.")
+          }else {
+            addTodoView();
+          }
         }}
       >
         확인
@@ -33,7 +56,9 @@ function Home() {
       <br />
       {/* map메소드로 데이터 배열을 돌면서 데이터를 화면에 출력함 */}
       {todoView.map((todo) => (
-        <TodoView id={1} name={todo} />
+        <TodoView key={todo.id} id={todo.id} name={todo.name} 
+          onDelete={todoDelete}
+        />
       ))}
     </div>
   );
--- src/pages/TodoView.tsx
+++ src/pages/TodoView.tsx
@@ -5,13 +5,16 @@
 interface TodoViewProps {
   id: number;
   name: string;
+  onDelete: (id: number) => void
 }
 
 function TodoView(props: TodoViewProps) {
+
   return (
     <div>
       {/* 문자열은 화면에 바로 출력 가능함 */}
       {props.name}
+      <button onClick={() => props.onDelete(props.id)}>삭제</button>
     </div>
   );
 }
