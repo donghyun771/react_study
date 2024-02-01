7b42e4c13d2caea1fd312fa7b380cb2d11156505
donghyun771 <dh031208@naver.com>
2024-02-01 21:53:28 +0900

    Prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,28 +12,30 @@ function Home() {
   const [todoView, setTodoView] = useState<TodoProps[]>([]);
 
   const addTodoView = useCallback(() => {
-
-    const todo = [...todoView]
+    const todo = [...todoView];
 
     // console.log(todo[todo.length-1].id + 1)
 
     // 이전 상태값과 현재 입력값을 합친 배열을 set
     if (todoView.length === 0) {
-      setTodoView([...todoView , {id : 1, name : todoContent}]);
+      setTodoView([...todoView, { id: 1, name: todoContent }]);
     } else {
-      setTodoView([...todoView , {id : todo[todo.length-1].id + 1, name : todoContent}]);
+      setTodoView([
+        ...todoView,
+        { id: todo[todo.length - 1].id + 1, name: todoContent },
+      ]);
     }
     // 입력 필드 비우기
     setTodoContent("");
   }, [todoContent, todoView]);
 
-  const todoDelete = useCallback((id: number) => {
-    const deleted = todoView.filter((todo) => (
-        todo.id !== id
-    ))
-    setTodoView(deleted)
-
-  }, [todoView])
+  const todoDelete = useCallback(
+    (id: number) => {
+      const deleted = todoView.filter((todo) => todo.id !== id);
+      setTodoView(deleted);
+    },
+    [todoView]
+  );
 
   return (
     <div>
@@ -49,8 +51,8 @@ function Home() {
       <button
         onClick={() => {
           if (todoContent === "") {
-            alert("내용이 없습니다.")
-          }else {
+            alert("내용이 없습니다.");
+          } else {
             addTodoView();
           }
         }}
@@ -60,7 +62,10 @@ function Home() {
       <br />
       {/* map메소드로 데이터 배열을 돌면서 데이터를 화면에 출력함 */}
       {todoView.map((todo) => (
-        <TodoView key={todo.id} id={todo.id} name={todo.name} 
+        <TodoView
+          key={todo.id}
+          id={todo.id}
+          name={todo.name}
           onDelete={todoDelete}
         />
       ))}
